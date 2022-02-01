c76c07ab12fada4fabdcdde0e7ec91b03a1b2b1d
Stefan Inman <96351972+Anon-67@users.noreply.github.com>
2022-02-01 09:40:18 -0600

    commit tree a063d786daae7614fedd971ab98dbdcb9a0e1355

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 2;
+let num2 =  31;
+let multiply = num1 * num2;
 
-// Write your code here
+
+
+let random = Math.ceil(Math.random());
+
+
+let num3 =5;
+let num4 =24;
+let mod = num4 % num3;
+
+let max = Math.max(1,5,10,20);
